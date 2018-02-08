6a05dbf45c6b61a6d0d9c9724cb69bf67fd1665f
Devin Nanayakkara <dvn14@point09.doc.ic.ac.uk>
2018-02-08 23:17:43 +0000

    Part 4 i, is partially done

---
+++ Part 4/example.pl
@@ -0,0 +1,5 @@
+myAsm(a).
+myAsm(b).
+contrary(a,p).
+myRule(p,[b]).
+myRule(p,[]).
---
+++ Part 4/p4.pl
@@ -0,0 +1,62 @@
+assumptions(A)	:- findall(X,myAsm(X),A).
+null_supports(N):- findall(X,myRule(X,[]),N).
+rules(R)		:- findall((X,Y),myRule(X,Y),R).
+
+add_assumptions([],A,A) :- !.
+add_assumptions([H|T],A,All1) :-
+    append(A,[(H,[H])],A1),
+    add_assumptions(T,A1,All1).
+
+add_nullsupports([],A,A) :- !.
+add_nullsupports([H|T],A,All2) :-
+    append(A,[(H,[])],A1),
+    add_nullsupports(T,A1,All2).
+
+notnullset(_,[],A,A) :- !.
+notnullset(NS,[H|T],N,F) :-
+	member(H,NS), !,
+    notnullset(NS,T,N,F).
+notnullset(NS,[H|T],N,F) :-
+    append(N,[H],N1),
+    notnullset(NS,T,N1,F).
+
+add_other(2,[],A,A) :- !.
+add_other(2,[(_,[])|T],A,AF) :- !,
+    add_other(2,T,A,AF).
+add_other(2,[(R,S)|T],A,AF) :-
+    null_supports(NS),
+    notnullset(NS,S,[],F),
+    append(A,[(R,F)],A1),
+    add_other(2,T,A1,AF).
+
+add_other(1,A,AF) :-
+    rules(R),
+    add_other(2,R,A,AF).
+
+all_arguments(All) :-
+    assumptions(A),
+    add_assumptions(A,[],All1),
+    null_supports(N),
+	add_nullsupports(N,All1,All2),
+    add_other(1,All2,All).
+
+get_set(_,[],S,S) :- !.
+get_set(C,[(C,[])|T],S,SF) :-
+    append(S,[[]],S1), !,
+    get_set(C,T,S1,SF).
+get_set(C,[(C,A)|T],S,SF) :-
+    append(S,[A],S1), !,
+    get_set(C,T,S1,SF).
+get_set(C,[_|T],S,SF) :-
+    get_set(C,T,S,SF).
+
+arguments_of_claim(C,Set) :-
+    all_arguments(All),
+    get_set(C,All,[],Set).
+
+argument((C,X)) :-
+    arguments_of_claim(C,Set),
+    member(X,Set), !.
+
+argument((C,Set)) :-
+    arguments_of_claim(C,Set).
